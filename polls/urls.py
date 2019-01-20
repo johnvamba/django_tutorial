76b6346c9ee421641506ab22fbacf93a91964edb
John Vincent Amba <johnvamba@gmail.com>
2019-01-20 16:04:58 +0800

    uuid fix; render views

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<uuid:question_id>/', views.detail, name='detail'),
+    path('<uuid:question_id>/results/', views.results, name='results'),
+    path('<uuid:question_id>/vote/', views.vote, name='vote'),
 ]
